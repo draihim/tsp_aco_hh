9c7fb7e63d17b3702840203bd8b211024b26c109
Miłosz Mazur <milosz.mazur@hotmail.com>
2014-12-17 15:03:37 +0100

    removing not needed comment

--- src/Main.java
+++ src/Main.java
@@ -10,20 +10,6 @@ public class Main {
     public static final int ITERATIONS = 100;
     public static final int ANT_SIZE = 10;
 
-    //test BFS
-    /*	public static void main(String[] args)
-    {
-		engine.AcoAlg alg = new engine.AcoAlg(ITERATIONS, ANT_SIZE);
-		alg.solve();
-		String fileName="res/a1.txt";
-		data.Graph graph=new data.Graph(fileName);
-		List<data.Vertex> vertexs=graph.getVertexs();
-		List<data.Vertex> bfs=graph.BFS(vertexs.get(0));
-		for(data.Vertex n:bfs){
-			System.out.println("("+n.getP().x+", "+n.getP().y+")");		
-		}
-	}*/
-    //test undirected graph
     public static void main(String[] args) {
         String fileName = "res/a1.txt";
         Graph graph = new Graph(fileName);
